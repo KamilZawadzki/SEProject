23962dac3b61bb541a61d38d8494ccc678de30c2
Marcin Frasz <noahy21@gmail.com>
2018-05-22 19:15:26 +0200

    Zmiana GUI (ListBox na checkedListBox)

--- SEProject/SEProject/Charts.Designer.cs
+++ SEProject/SEProject/Charts.Designer.cs
@@ -28,41 +28,33 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.listBox2 = new System.Windows.Forms.ListBox();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
             this.button2 = new System.Windows.Forms.Button();
-            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
             this.groupBox1.SuspendLayout();
             this.groupBox2.SuspendLayout();
             this.SuspendLayout();
             // 
-            // listBox2
-            // 
-            this.listBox2.FormattingEnabled = true;
-            this.listBox2.ItemHeight = 16;
-            this.listBox2.Location = new System.Drawing.Point(6, 21);
-            this.listBox2.Name = "listBox2";
-            this.listBox2.Size = new System.Drawing.Size(266, 308);
-            this.listBox2.TabIndex = 1;
-            // 
             // textBox1
             // 
             this.textBox1.ForeColor = System.Drawing.SystemColors.AppWorkspace;
-            this.textBox1.Location = new System.Drawing.Point(6, 337);
-            this.textBox1.Margin = new System.Windows.Forms.Padding(5);
+            this.textBox1.Location = new System.Drawing.Point(4, 274);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(266, 22);
+            this.textBox1.Size = new System.Drawing.Size(200, 20);
             this.textBox1.TabIndex = 2;
             this.textBox1.Text = "Wyszukaj...";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(6, 367);
+            this.button1.Location = new System.Drawing.Point(4, 298);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(266, 32);
+            this.button1.Size = new System.Drawing.Size(200, 26);
             this.button1.TabIndex = 3;
             this.button1.Text = "Wyczyść zaznaczenie";
             this.button1.UseVisualStyleBackColor = true;
@@ -70,41 +62,21 @@
             // groupBox1
             // 
             this.groupBox1.Controls.Add(this.listBox1);
-            this.groupBox1.Location = new System.Drawing.Point(22, 12);
+            this.groupBox1.Location = new System.Drawing.Point(16, 10);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(316, 481);
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.groupBox1.Size = new System.Drawing.Size(237, 391);
             this.groupBox1.TabIndex = 5;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Dostępne szablony wykresów";
             this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
-            // 
-            // groupBox2
-            // 
-            this.groupBox2.Controls.Add(this.button1);
-            this.groupBox2.Controls.Add(this.textBox1);
-            this.groupBox2.Controls.Add(this.listBox2);
-            this.groupBox2.Location = new System.Drawing.Point(344, 12);
-            this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(278, 412);
-            this.groupBox2.TabIndex = 6;
-            this.groupBox2.TabStop = false;
-            this.groupBox2.Text = "Atrybuty";
-            // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(350, 461);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(266, 32);
-            this.button2.TabIndex = 7;
-            this.button2.Text = "Wygeneruj wykres";
-            this.button2.UseVisualStyleBackColor = true;
             // 
             // listBox1
             // 
             this.listBox1.FormattingEnabled = true;
             this.listBox1.HorizontalScrollbar = true;
             this.listBox1.ImeMode = System.Windows.Forms.ImeMode.NoControl;
-            this.listBox1.ItemHeight = 16;
             this.listBox1.Items.AddRange(new object[] {
             "Ilość sprzedanych telefonów z podziałem na ich marki",
             "Ilość sprzedanych laptopów z podziałem na ich marki",
@@ -112,21 +84,55 @@
             "Porównanie 2 wybranych laptopów pod względem wybranego parametru",
             "Ilość laptopów zalegających w magazynie wybranego jednego producenta",
             "Ilość laptopów zalegających w magazynie wybranego jednego producenta"});
-            this.listBox1.Location = new System.Drawing.Point(6, 21);
+            this.listBox1.Location = new System.Drawing.Point(4, 17);
+            this.listBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.listBox1.Name = "listBox1";
             this.listBox1.ScrollAlwaysVisible = true;
-            this.listBox1.Size = new System.Drawing.Size(304, 452);
+            this.listBox1.Size = new System.Drawing.Size(229, 368);
             this.listBox1.TabIndex = 0;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.checkedListBox1);
+            this.groupBox2.Controls.Add(this.button1);
+            this.groupBox2.Controls.Add(this.textBox1);
+            this.groupBox2.Location = new System.Drawing.Point(258, 10);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.groupBox2.Size = new System.Drawing.Size(208, 335);
+            this.groupBox2.TabIndex = 6;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Atrybuty";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(262, 375);
+            this.button2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(200, 26);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Wygeneruj wykres";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Location = new System.Drawing.Point(5, 17);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(198, 244);
+            this.checkedListBox1.TabIndex = 4;
             // 
             // Charts
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(643, 524);
+            this.ClientSize = new System.Drawing.Size(482, 426);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Charts";
             this.Text = "Wykresy";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -138,13 +144,13 @@
         }
 
         #endregion
-        private System.Windows.Forms.ListBox listBox2;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.GroupBox groupBox2;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
     }
 }
 
