20fd5f2ea94f3c4520cf0ab6f0618715c536b919
Marcin Frasz <noahy21@gmail.com>
2018-05-22 21:18:35 +0200

    Obsługa przycisków +Diagram.cs

--- SEProject/SEProject/Charts.Designer.cs
+++ SEProject/SEProject/Charts.Designer.cs
@@ -28,40 +28,43 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.button1 = new System.Windows.Forms.Button();
+            this.textBox_wyszukaj = new System.Windows.Forms.TextBox();
+            this.button_wyczysc_zazn = new System.Windows.Forms.Button();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox_szablony = new System.Windows.Forms.ListBox();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
-            this.button2 = new System.Windows.Forms.Button();
-            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.button_generuj_wykres = new System.Windows.Forms.Button();
+            this.listBox_atrybuty = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.listBox_model = new System.Windows.Forms.ListBox();
             this.groupBox1.SuspendLayout();
             this.groupBox2.SuspendLayout();
             this.SuspendLayout();
             // 
-            // textBox1
+            // textBox_wyszukaj
             // 
-            this.textBox1.ForeColor = System.Drawing.SystemColors.AppWorkspace;
-            this.textBox1.Location = new System.Drawing.Point(4, 274);
-            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(200, 20);
-            this.textBox1.TabIndex = 2;
-            this.textBox1.Text = "Wyszukaj...";
+            this.textBox_wyszukaj.ForeColor = System.Drawing.SystemColors.AppWorkspace;
+            this.textBox_wyszukaj.Location = new System.Drawing.Point(4, 274);
+            this.textBox_wyszukaj.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox_wyszukaj.Name = "textBox_wyszukaj";
+            this.textBox_wyszukaj.Size = new System.Drawing.Size(200, 20);
+            this.textBox_wyszukaj.TabIndex = 2;
+            this.textBox_wyszukaj.Text = "Wyszukaj...";
             // 
-            // button1
+            // button_wyczysc_zazn
             // 
-            this.button1.Location = new System.Drawing.Point(4, 298);
-            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(200, 26);
-            this.button1.TabIndex = 3;
-            this.button1.Text = "Wyczyść zaznaczenie";
-            this.button1.UseVisualStyleBackColor = true;
+            this.button_wyczysc_zazn.Location = new System.Drawing.Point(4, 298);
+            this.button_wyczysc_zazn.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button_wyczysc_zazn.Name = "button_wyczysc_zazn";
+            this.button_wyczysc_zazn.Size = new System.Drawing.Size(200, 26);
+            this.button_wyczysc_zazn.TabIndex = 3;
+            this.button_wyczysc_zazn.Text = "Wyczyść zaznaczenie";
+            this.button_wyczysc_zazn.UseVisualStyleBackColor = true;
+            this.button_wyczysc_zazn.Click += new System.EventHandler(this.button_wyczysc_zazn_Click);
             // 
             // groupBox1
             // 
-            this.groupBox1.Controls.Add(this.listBox1);
+            this.groupBox1.Controls.Add(this.listBox_szablony);
             this.groupBox1.Location = new System.Drawing.Point(16, 10);
             this.groupBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.groupBox1.Name = "groupBox1";
@@ -72,30 +75,33 @@
             this.groupBox1.Text = "Dostępne szablony wykresów";
             this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
             // 
-            // listBox1
+            // listBox_szablony
             // 
-            this.listBox1.FormattingEnabled = true;
-            this.listBox1.HorizontalScrollbar = true;
-            this.listBox1.ImeMode = System.Windows.Forms.ImeMode.NoControl;
-            this.listBox1.Items.AddRange(new object[] {
+            this.listBox_szablony.FormattingEnabled = true;
+            this.listBox_szablony.HorizontalScrollbar = true;
+            this.listBox_szablony.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.listBox_szablony.Items.AddRange(new object[] {
             "Ilość sprzedanych telefonów z podziałem na ich marki",
             "Ilość sprzedanych laptopów z podziałem na ich marki",
             "Porównanie 2 wybranych telefonów pod względem wybranego parametru",
             "Porównanie 2 wybranych laptopów pod względem wybranego parametru",
             "Ilość laptopów zalegających w magazynie wybranego jednego producenta",
             "Ilość laptopów zalegających w magazynie wybranego jednego producenta"});
-            this.listBox1.Location = new System.Drawing.Point(4, 17);
-            this.listBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.listBox1.Name = "listBox1";
-            this.listBox1.ScrollAlwaysVisible = true;
-            this.listBox1.Size = new System.Drawing.Size(229, 368);
-            this.listBox1.TabIndex = 0;
+            this.listBox_szablony.Location = new System.Drawing.Point(4, 17);
+            this.listBox_szablony.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.listBox_szablony.Name = "listBox_szablony";
+            this.listBox_szablony.ScrollAlwaysVisible = true;
+            this.listBox_szablony.Size = new System.Drawing.Size(229, 368);
+            this.listBox_szablony.TabIndex = 0;
+            this.listBox_szablony.SelectedIndexChanged += new System.EventHandler(this.listBox_szablony_SelectedIndexChanged);
             // 
             // groupBox2
             // 
-            this.groupBox2.Controls.Add(this.checkedListBox1);
-            this.groupBox2.Controls.Add(this.button1);
-            this.groupBox2.Controls.Add(this.textBox1);
+            this.groupBox2.Controls.Add(this.listBox_model);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Controls.Add(this.listBox_atrybuty);
+            this.groupBox2.Controls.Add(this.button_wyczysc_zazn);
+            this.groupBox2.Controls.Add(this.textBox_wyszukaj);
             this.groupBox2.Location = new System.Drawing.Point(258, 10);
             this.groupBox2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.groupBox2.Name = "groupBox2";
@@ -105,30 +111,53 @@
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "Atrybuty";
             // 
-            // button2
+            // button_generuj_wykres
             // 
-            this.button2.Location = new System.Drawing.Point(262, 375);
-            this.button2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(200, 26);
-            this.button2.TabIndex = 7;
-            this.button2.Text = "Wygeneruj wykres";
-            this.button2.UseVisualStyleBackColor = true;
+            this.button_generuj_wykres.Location = new System.Drawing.Point(262, 375);
+            this.button_generuj_wykres.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button_generuj_wykres.Name = "button_generuj_wykres";
+            this.button_generuj_wykres.Size = new System.Drawing.Size(200, 26);
+            this.button_generuj_wykres.TabIndex = 7;
+            this.button_generuj_wykres.Text = "Wygeneruj wykres";
+            this.button_generuj_wykres.UseVisualStyleBackColor = true;
+            this.button_generuj_wykres.Click += new System.EventHandler(this.button_generuj_wykres_Click);
             // 
-            // checkedListBox1
+            // listBox_atrybuty
             // 
-            this.checkedListBox1.FormattingEnabled = true;
-            this.checkedListBox1.Location = new System.Drawing.Point(5, 17);
-            this.checkedListBox1.Name = "checkedListBox1";
-            this.checkedListBox1.Size = new System.Drawing.Size(198, 244);
-            this.checkedListBox1.TabIndex = 4;
+            this.listBox_atrybuty.FormattingEnabled = true;
+            this.listBox_atrybuty.HorizontalScrollbar = true;
+            this.listBox_atrybuty.Location = new System.Drawing.Point(15, 18);
+            this.listBox_atrybuty.Name = "listBox_atrybuty";
+            this.listBox_atrybuty.ScrollAlwaysVisible = true;
+            this.listBox_atrybuty.Size = new System.Drawing.Size(187, 108);
+            this.listBox_atrybuty.TabIndex = 4;
+            this.listBox_atrybuty.SelectedIndexChanged += new System.EventHandler(this.listBox_atrybuty_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 143);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(36, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Model";
+            // 
+            // listBox_model
+            // 
+            this.listBox_model.FormattingEnabled = true;
+            this.listBox_model.HorizontalScrollbar = true;
+            this.listBox_model.Location = new System.Drawing.Point(15, 159);
+            this.listBox_model.Name = "listBox_model";
+            this.listBox_model.ScrollAlwaysVisible = true;
+            this.listBox_model.Size = new System.Drawing.Size(187, 95);
+            this.listBox_model.TabIndex = 6;
             // 
             // Charts
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(482, 426);
-            this.Controls.Add(this.button2);
+            this.ClientSize = new System.Drawing.Size(472, 426);
+            this.Controls.Add(this.button_generuj_wykres);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
@@ -144,13 +173,15 @@
         }
 
         #endregion
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox_wyszukaj;
+        private System.Windows.Forms.Button button_wyczysc_zazn;
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.GroupBox groupBox2;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.ListBox listBox1;
-        private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.Button button_generuj_wykres;
+        private System.Windows.Forms.ListBox listBox_szablony;
+        private System.Windows.Forms.ListBox listBox_atrybuty;
+        private System.Windows.Forms.ListBox listBox_model;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- SEProject/SEProject/Charts.cs
+++ SEProject/SEProject/Charts.cs
@@ -7,13 +7,30 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
 
 namespace SEProject
 {
     public partial class Charts : Form
     {
+        private MySqlConnection conn;
+        private string server;
+        private string database;
+        private string uid;
+        private string password;
+        public static string username;
+        public static string operation;
+
         public Charts()
         {
+            server = "localhost";
+            database = "electronicstore_projekt";
+            uid = "root";
+            password = "";
+            string connString;
+            connString = $"SERVER={server};DATABASE={database};UID={uid};PASSWORD={password};";
+            conn = new MySqlConnection(connString);
+            
             InitializeComponent();
         }
 
@@ -30,6 +47,58 @@ namespace SEProject
         private void listView1_SelectedIndexChanged(object sender, EventArgs e)
         {
 
+        }
+
+        private void checkedListBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button_wyczysc_zazn_Click(object sender, EventArgs e)
+        {
+            listBox_atrybuty.ClearSelected();
+            listBox_szablony.ClearSelected();
+        }
+
+        private void listBox_atrybuty_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void listBox_szablony_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button_generuj_wykres_Click(object sender, EventArgs e)
+        {
+            
+            switch (listBox_szablony.SelectedIndex)
+            {
+                case 0:
+                    MessageBox.Show("option 1");
+                    
+                    break;
+                case 1:
+                    MessageBox.Show("option 2");
+                    break;
+                case 2:
+                    MessageBox.Show("option 3");
+                    break;
+                case 3:
+                    MessageBox.Show("option 4");
+                    break;
+                case 4:
+                    MessageBox.Show("option 5");
+                    break;
+                case 5:
+                    MessageBox.Show("option 6");
+                    break;
+                default:
+                   MessageBox.Show("Nie wybrano szablonu!"); 
+                    break;
+
+            }
         }
     }
 }
